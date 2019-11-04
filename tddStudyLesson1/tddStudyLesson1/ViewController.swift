fa807fbb89037d9c3a443293a81dbda8164b84b9
Steven Jiang <jshl8612@gmail.com>
2019-11-04 21:29:33 +0900

    [Init] add Lesson1 project file

---
+++ tddStudyLesson1/tddStudyLesson1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tddStudyLesson1
+//
+//  Created by 강성용 on 04/11/2019.
+//  Copyright © 2019 StevenJiang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ tddStudyLesson1/tddStudyLesson1Tests/tddStudyLesson1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  tddStudyLesson1Tests.swift
+//  tddStudyLesson1Tests
+//
+//  Created by 강성용 on 04/11/2019.
+//  Copyright © 2019 StevenJiang. All rights reserved.
+//
+
+import XCTest
+@testable import tddStudyLesson1
+
+class tddStudyLesson1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
